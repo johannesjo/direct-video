4ca78f3870982c8a65065301a6a4b8992a4e308c
Johannes Millan <johannes.millan@gmail.com>
2017-05-02 19:25:12 +0200

    add handling of object

--- get-video-urls.js
+++ get-video-urls.js
@@ -1,13 +1,17 @@
 function getVideoUrls() {
-  console.log('I am here!');
-
   const videoUrls = [];
   let videoEls = document.querySelectorAll('video, object, embed');
 
   for (let i = 0; i < videoEls.length; i++) {
     let videoEl = videoEls[i];
-    let src = videoEl.currentSrc.replace('blob:', '');
-    videoUrls.push(src);
+    console.log(videoEl, videoEl.tagName);
+    if (videoEl.tagName === 'VIDEO') {
+      videoUrls.push(videoEl.currentSrc.replace('blob:', ''));
+    } else if (videoEl.tagName === 'OBJECT') {
+      videoUrls.push(videoEl.getAttribute('data'));
+    } else if (videoEl.tagName === 'EMBED') {
+      
+    }
   }
   return videoUrls;
 }
